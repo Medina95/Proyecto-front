f5d9ac6cbada7121859c292ccb3515030f2cad74
ChristianDuarteR <christianjavierduarte@gmail.com>
2024-11-22 18:13:25 -0500

    Integracion con BackEnd Realizada

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,11 @@ import {Face} from "./components/Face";
 
 
   export function App() {
-    const [porcentaje1, setPorcentaje1] = useState(30);
-    const [porcentaje2, setPorcentaje2] = useState(60);
-    const [porcentaje3, setPorcentaje3] = useState(90);
 
-    const [porcentages, setPorcentages]  = useState([]); // Este fetch trae los porcentajes
+    const [porcentages, setPorcentages]  = useState([]);
 
     useEffect(() => {
-      const apiUrl = "AquiDeberiaHaberUnaURl:8080";
+      const apiUrl = "http://localhost:8080/api/v1/reviews/porcentajes";
 
       const fetchPorcetages = async () => {
         try {
@@ -36,17 +33,11 @@ import {Face} from "./components/Face";
         <div className="App">
           <h1>Caritas con porcentaje</h1>
           <div className="caritas-container">
-            <Face porcentage={porcentaje1} name={"carita feliz"} mood={"carita-mouth happy-mouth"} color={'green'}/>
-            <Face porcentage={porcentaje2} name={"carita neutral"} mood={"carita-mouth neutral-mouth"} color={'gray'}/>
-            <Face porcentage={porcentaje3} name={"carita sad"} mood={"carita-mouth sad-mouth"} color={'red'}/>
+            <Face porcentage={porcentages[1]} name={"carita feliz"} mood={"carita-mouth happy-mouth"} color={'green'}/>
+            <Face porcentage={porcentages[2]} name={"carita sad"} mood={"carita-mouth sad-mouth"} color={'red'}/>
 
           </div>
 
-          <div className="controls">
-            <button onClick={() => setPorcentaje1(porcentaje1 + 10)}>Aumentar Carita 1</button>
-            <button onClick={() => setPorcentaje2(porcentaje2 + 10)}>Aumentar Carita 2</button>
-            <button onClick={() => setPorcentaje3(porcentaje3 + 10)}>Aumentar Carita 3</button>
-          </div>
 
           <div className="App">
         <h1>D3.js Pie Chart y Word Cloud</h1>
